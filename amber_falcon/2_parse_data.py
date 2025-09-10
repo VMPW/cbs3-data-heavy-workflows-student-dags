3e4c60b3830a367d9e0a2860956d9f2db9d45415
davidciz <david.ciz@vsb.cz>
2025-09-10 11:07:43 +0200

    First push to add first DAG to all students

--- amber_falcon/2_parse_data.py
+++ amber_falcon/2_parse_data.py
@@ -107,7 +107,7 @@ with DAG(
     )
     resolve = PythonOperator(
         task_id="resolve_inputs",
-        python_callable=resolve_inputs,
+        python_callable=OH_NO_THIS_IS_NOT_CORRECT_AT_ALL_THIS_SHOULD_CALL_A_FUNCTION_NOT_BE_A_STRING,
     )
 
     fetch_data >> resolve
